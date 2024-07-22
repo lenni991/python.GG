bc6b29101c4e163a42e1fa6552450f2c2ab549a7
Mohamed Soliman <82563121+lenni991@users.noreply.github.com>
2024-07-22 14:05:59 +0300

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -7,20 +7,3 @@ class Solution:
 
 reuslt = Solution().two_sum([2, 7, 11, 15], 9)
 print(reuslt)
-
-
-#############################
-# class Solution:
-#     def twoSum(self, nums, target):
-#         num_to_index = {} 
-#         for i, num in enumerate(nums):
-#             if target - num in num_to_index:
-#                 return [num_to_index[target - num], i]
-#             num_to_index[num] = i
-#         return []
-
-
-# #------------------------------
-# #------------------------------
-
-
